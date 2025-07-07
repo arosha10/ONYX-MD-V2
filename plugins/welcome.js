3a97ac6798cb3ee61399d7cdfc36ac2d42cd4a51
Arosh Samuditha <108263734+aroshsamuditha@users.noreply.github.com>
2025-07-07 14:30:25 +0530

    Update welcome.js

--- plugins/welcome.js
+++ plugins/welcome.js
@@ -18,7 +18,7 @@ module.exports = async function (robin, groupId, newMembers) {
 
     for (const member of newMembers) {
       // Custom welcome message
-      const caption = `👋😍 *WELCOME TO ${groupName}!*\n@${member.split("@")[0]} 👻, we are glad to have you here!\n\n*Please be sure to follow the rules of the group you are in!*\n------------------------------------\n> Also, a 🤖bot has been installed for group administration, so please refrain from sharing obscene material or sharing other group links😾🖐\n\n> *🌀ONYX MD🔥BOT👾BY AROSH*`;
+      const caption = `👋😍 *WELCOME TO ${groupName}!*\n@${member.split("@")[0]} 👻,\n ∧,,,∧\n(  ̳• · • ̳)\n /    づ♡ I love you\n\nwe are glad to have you here!\n\n*Please be sure to follow the rules of the group you are in!*\n------------------------------------\n> Also, a 🤖bot has been installed for group administration, so please refrain from sharing obscene material or sharing other group links😾🖐\n\n> *🌀ONYX MD🔥BOT👾BY AROSH*`;
 
       // Send image with caption
       await robin.sendMessage(groupId, {
